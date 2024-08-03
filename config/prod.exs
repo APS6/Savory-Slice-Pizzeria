f786c15c54d2b1d88a4a52a4cb9f2199b34ded07
APS <anirudhapsah@gmail.com>
2024-08-03 17:42:10 +0530

    maybe this?

--- config/prod.exs
+++ config/prod.exs
@@ -17,19 +17,19 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
-# config :savory_slice, SavorySliceWeb.Endpoint,
-#   # Possibly not needed, but doesn't hurt
-#   http: [port: {:system, "PORT"}],
-#   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-#   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-#   server: true
+config :savory_slice, SavorySliceWeb.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
 
-# config :savory_slice, SavorySlice.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   ssl: true,
-#   # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-#   pool_size: 2
+config :savory_slice, SavorySlice.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
