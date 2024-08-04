45da1e3088faa5781bc1d5ddd93858b9041d718e
APS <anirudhapsah@gmail.com>
2024-08-04 16:47:41 +0530

    set host to pizza.anirudha.pro

--- config/prod.exs
+++ config/prod.exs
@@ -9,7 +9,7 @@ config :savory_slice, SavorySliceWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  url: [host: "pizza.anirudha.pro", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
 
